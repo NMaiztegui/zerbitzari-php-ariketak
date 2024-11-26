ac4f254d59a317d26d2642361518bd09035497f6
Naroa Maiztegui <naroamaiztegui@gmail.com>
2024-11-26 13:26:21 +0100

    tabla pantailarstze saiakera

--- PHP-MYSQL-Ariketak/tabla-ariketa.php
+++ PHP-MYSQL-Ariketak/tabla-ariketa.php
@@ -10,6 +10,24 @@
     echo "Error de conexión a la BD: ".mysqli_connect_error();
     exit();
     }
+
+
+    $query="SELECT * FROM etxe_bizitzak";
+    $resultado=$mysqli->query($query)
+          or die ($mysqli ->error. " en la línea ".(__LINE__‐1));
+
+    $numregistros=$resultado ->num_rows;
+    echo "<p>El número de pisos es: ",$numregistros,".</p>";
+    echo "<table border=2><tr><th>mota</th> <th>zonaldea</th> <th>helbidea</th>
+    <th>logelak</th> <th>prezioa</th> <th>tamaina</th> <th>extrak</th> <th>irudia</th> <th>oharrak</th></tr>";
+    while ($registro = $resultado ->fetch_assoc()) {
+        echo "<tr>";
+        foreach ($registro as $campo)
+        echo "<td>",$campo, "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+    $resultado ->free();
     
 ?>
 <!DOCTYPE html>
